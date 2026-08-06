1c0e977aee1b7412203d98c4991c99859c57323e
juv4uk <85576956+juv4uk@users.noreply.github.com>
2026-08-06 23:25:29 +0300

    Bump version to 1.0.6 and add versioning to executables

---
+++ build-all.ps1
@@ -0,0 +1,27 @@
+$ErrorActionPreference = "Stop"
+
+$proj = Get-Content project.clj | Out-String
+if ($proj -match '\(defproject\s+\S+\s+"([^"]+)"') {
+    $version = $matches[1]
+} else {
+    $version = "1.0.0"
+}
+Write-Host "Detected version: $version"
+
+Write-Host "Building for Windows..."
+clj -M:prod uberjar windows
+Move-Item -Path "target\clojure-code-$version-standalone.jar" -Destination "target\clojure-code-windows.jar" -Force
+if (Get-Command jpackage -ErrorAction SilentlyContinue) {
+    Write-Host "Creating Windows executable with jpackage..."
+    jpackage --type app-image --name "Clojure-code-$version" --app-version $version --input target/ --main-jar clojure-code-windows.jar --main-class nightcode.start --icon package/windows/Nightcode.ico
+}
+
+Write-Host "Building for macOS..."
+clj -M:prod uberjar macos
+Move-Item -Path "target\clojure-code-$version-standalone.jar" -Destination "target\clojure-code-macos.jar" -Force
+
+Write-Host "Building for Linux..."
+clj -M:prod uberjar linux
+Move-Item -Path "target\clojure-code-$version-standalone.jar" -Destination "target\clojure-code-linux.jar" -Force
+
+Write-Host "Build complete!"
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-code "1.0.0"
+(defproject clojure-code "1.0.6"
   :description "Clojure-code: A modernised fork of the Nightcode IDE"
   :url "https://github.com/juv4uk/Clojure-code"
   :license {:name "Public Domain"
