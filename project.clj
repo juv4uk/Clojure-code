3a3e3f73c797ca38284d23004f0fc118b4ad7952
juv4uk <85576956+juv4uk@users.noreply.github.com>
2026-08-07 01:46:09 +0300

    Fix repl launch by adding clojure to dependencies in project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
-(defproject clojure-code "1.1.0"
+(defproject clojure-code "1.1.1"
   :description "Clojure-code: A modernised fork of the Nightcode IDE"
   :url "https://github.com/juv4uk/Clojure-code"
   :license {:name "Public Domain"
             :url "http://unlicense.org/UNLICENSE"}
+  :dependencies [[org.clojure/clojure "1.11.2"]]
+  :source-paths ["src/clj" "src/cljs"]
   :repositories [["clojars" {:url "https://clojars.org/repo"
                              :sign-releases false}]])
